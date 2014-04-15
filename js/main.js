6d9e16a7e4c856d0299f7648eaa0daa611be1cb0
Michael Perez <michaelp193@gmail.com>
2014-04-15 18:22:52 -0400

    restructured, moved to sass

--- js/main.js
+++ js/main.js
@@ -1,39 +1,32 @@
 $(document).ready(function(){
-	$("nav").find("li").click(function(){
-		console.log("Hey you're clickin" + $(this.id));
+
+	function slidePane(buttonId) {
+		var pane = $(buttonId);
+		var body = $("body");
+		function open() {
+			pane.toggleClass('activated');
+			body.toggleClass("open");
+		}
+		if(pane.siblings().hasClass("activated")) {
+			body.removeClass("open");
+			pane.siblings().removeClass("activated");
+			open();
+		} else {
+			open();
+		}
+		console.log(buttonId , "pressed");
+	}
+	
+	$(".menu").find(".menuItem").click(function(){
 		switch(this.id) {
 			case "games":
-				if($("#gameInfo").siblings().hasClass("activated")) 
-				{
-					$("body").removeClass("open");
-					$("#gameInfo").siblings().removeClass("activated");
-				}
-				console.log("Game pressed");
-				$("#gameInfo").toggleClass('activated');
-				$("body").toggleClass("open");
-				window.location = "#games";
+				slidePane("#gameInfo");
 				break;
 			case "about":
-				if($("#aboutInfo").siblings().hasClass("activated")) 
-				{
-					$("body").removeClass("open");
-					$("#aboutInfo").siblings().removeClass("activated");
-				}
-				console.log("About pressed");	
-				$("#aboutInfo").toggleClass('activated');
-				$("body").toggleClass("open");
-				window.location = "#about";
+				slidePane("#aboutInfo");
 				break;
 			case "contact":
-				if($("#contactInfo").siblings().hasClass("activated"))
-				{
-					$("body").removeClass("open");
-					$("#contactInfo").siblings().removeClass("activated");
-				}
-				console.log("Contact pressed");
-				$("#contactInfo").toggleClass('activated');
-				$("body").toggleClass("open");
-				window.location = "#contact";
+				slidePane("#contactInfo");
 				break;
 			default:
 				console.log(" defaults ");
